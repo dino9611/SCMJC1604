df330ea557702225a2ca04b7652db97ff2b73a39
dino9611 <45329868+dino9611@users.noreply.github.com>
2021-05-06 12:01:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,3 +37,15 @@ console.log("feature 2");
 var a = "ini feature 2 bro blblabala";
 
 console.log(a);
+console.log("ini feature");
+console.log("dari contributor lain");
+
+console.log("feature 1");
+
+var b = 1234;
+
+console.log(b + b);
+
+console.log(000);
+console.log("feature 2");
+var a = "ini feature 2 bro blblabala";
