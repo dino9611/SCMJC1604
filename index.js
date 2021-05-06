a8c593c0261624e94fef63c0da7b66506916412a
dino9611 <aldinorahman36@gmail.com>
2021-05-06 10:29:01 +0700

    feature-2 update

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-console.log('ini feature')
-console.log("dari contributor lain")
+console.log("ini feature");
+console.log("dari contributor lain");
 
-console.log("feature 2")
+console.log("feature 2");
+var a = "ini feature 2 bro blblabala";
+
+console.log(a);
