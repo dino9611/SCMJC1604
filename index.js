913ac38b3c60021e43bbcc2ee6cded3a1fde3036
dino9611 <aldinorahman36@gmail.com>
2021-05-06 11:36:35 +0700

    addd feature 4

--- index.js
+++ index.js
@@ -23,3 +23,17 @@ console.log("dadsada");
 console.log("dadsada");
 console.log("dadsada");
 console.log("dadsada");
+console.log("ini feature");
+console.log("dari contributor lain");
+
+console.log("feature 1");
+
+var b = 1234;
+
+console.log(b + b);
+
+console.log(000);
+console.log("feature 2");
+var a = "ini feature 2 bro blblabala";
+
+console.log(a);
